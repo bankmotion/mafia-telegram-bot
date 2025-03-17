8efda49745cd6b63a86e7f4d061b9325ad569212
bankmotion <harris5555555555555@gmail.com>
2025-03-17 13:58:58 -0700

    UPDATE: bnb contract address

--- src/config/config.ts
+++ src/config/config.ts
@@ -36,18 +36,18 @@ export const Config = {
 export const Contract = {
   RankXP: {
     [ChainType.PLS]: "0x74eADd7ebeeED638FD7c413134FA3D3433699D92",
-    [ChainType.BNB]: "0xCeA74Da2020C42AbD1f0633ece2C3dcA7c45BC65",
+    [ChainType.BNB]: "0x48F2C9C0ea337854492aF5bEbEa74e8917712B71",
   },
   CarNick: {
     [ChainType.PLS]: "0x2bf1EEaa4e1D7502AeF7f5beCCf64356eDb4a8c8",
-    [ChainType.BNB]: "0x0E16b9B89A19C7b70F881c18c80a8934587D6E9F",
+    [ChainType.BNB]: "0x60B8e0dd9566b42F9CAa5538350aA0D29988373c",
   },
   Jail: {
     [ChainType.PLS]: "0xDCD5E9c0b2b4E9Cb93677A258521D854b3A9f5A1",
-    [ChainType.BNB]: "0x4376020D730d66222ae22140760599cf1FAd4B45",
+    [ChainType.BNB]: "0x7371580cd13dE739C734AE85062F75194d13Fac2",
   },
   Smuggle: {
     [ChainType.PLS]: "0x9bf722B3350832ae9023B7C9762227bE33943d09",
-    [ChainType.BNB]: "0xF2ec62776AC62901fE5Ce027bDa8d40eF8f01142",
+    [ChainType.BNB]: "0x36b09f1854CF3614Eb8d10fFae847511BB08868e",
   },
 };
